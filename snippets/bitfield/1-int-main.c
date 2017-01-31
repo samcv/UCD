448debae7d76ce6ccd849402b39361938b684365
Samantha McVey <samantham@posteo.net>
2017-01-30 16:06:39 -0800

    Add the start of a C function to get general category

--- snippets/bitfield/1-int-main.c
+++ snippets/bitfield/1-int-main.c
@@ -4,4 +4,13 @@
 /* Returns the value stored in the enum for a specified property. This will be the final property value */
 #define get_enum_prop(cp, propname) propname[get_cp_raw_value(cp, propname)]
 
+int get_gencat ( uint32_t cp ) {
+    char gc[3];
+    gc[2] = '\0';
+    gc[0] = get_enum_prop(cp, General_Category_1);
+    gc[1] =  get_enum_prop(cp, General_Category_2);
+    printf("%i", get_enum_prop(cp, General_Category_1));
+    printf("gencat %s\n", gc);
+}
+
 int main (void) {
